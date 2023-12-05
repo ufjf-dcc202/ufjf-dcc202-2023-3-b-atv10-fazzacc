40cb64add7af21999c9b82a8deba19b02902dfd7
Larissa <10475169697@estudante.ufjf.br>
2023-12-04 22:10:11 -0300

    Acessando as operações

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-import { getJoao, getMaria } from "./joaoEMaria.js";
+import { getJoao, getMaria, setMaria, setJoao, deJoaoParaMaria, deMariaParaJoao } from "./joaoEMaria.js";
 
 // main.js
 console.log('módulo main');
+
+console.log('João tem ' , getJoao(), ' maçãs!');
+console.log('Maria tem ' , getMaria(), ' maçãs!');
+
+deJoaoParaMaria();
 
 console.log('João tem ' , getJoao(), ' maçãs!');
 console.log('Maria tem ' , getMaria(), ' maçãs!');
